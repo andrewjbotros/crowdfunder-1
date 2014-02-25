1b67a9f9165ca03f4ba42556f531b02966c5b4a3
Scott Chu <domaru.tdot@gmail.com>
2014-02-25 11:44:06 -0500

    Add concern for session

---
+++ app/controllers/concerns/session.rb
@@ -0,0 +1,31 @@
+module Session
+  extend ActiveSupport::Concern
+
+private
+
+  def current_user
+    @current_user ||= User.find(token: User.encrypt(cookies[:token]))
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def signin(user)
+    token = User.new_token
+    cookies[:token] = { value: token, expires: 2.hour.from_now }
+    user.update_attribute(:token, User.encrypt(token))
+    self.current_user = user
+  end
+
+  def signout
+    current_user.update_attribute(:token, User.encrypt(User.new_token))
+    cookies[:token] = nil
+    self.current_user = nil
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+end
