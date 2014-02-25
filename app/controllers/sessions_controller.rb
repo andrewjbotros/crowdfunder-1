9a7ad567daca8bb4972151be85973988995cfa08
Scott Chu <domaru.tdot@gmail.com>
2014-02-25 11:33:07 -0500

    Add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user =  User.find(username: params[:session][:signin_id]) ||
+            User.find(email: params[:session][:signin_id])
+
+    if user && user.authenticate(params[:session][:password])
+      # signin user
+      flash[:info] = "You are now signed in"
+      redirect_to current_user
+    else
+      flash.now[:warning] = "Invalid username/email and password combination"
+      render :new
+  end
+
+  def destroy
+    # signout
+    flash[:info] = "You are now signed out"
+    redirect_to root_path
+  end
+
+end
