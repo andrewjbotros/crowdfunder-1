f08ed7093276ae646b9da4493e1795a2750e2aff
Scott Chu <domaru.tdot@gmail.com>
2014-02-25 11:35:54 -0500

    Update routes, add sessions resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Crowdfunder::Application.routes.draw do
+
+  get 'signin'          =>    'sessions#new',               as: :signin
+  get 'signout'         =>    'sessions#destroy',           as: :signout
+  resources :sessions, only: [:create]
+
+  get 'signup'          =>    'users#new',                  as: :signup
   resources :users
+
   root      'welcome#index'
 end
