d6bb8af02c04b9e9b51a3fd64d13996610405d0c
Scott Chu <domaru.tdot@gmail.com>
2014-02-26 13:16:53 -0500

    Add new pledge

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -3,10 +3,22 @@ class Pledge < ActiveRecord::Base
   belongs_to :user
   belongs_to :breakpoint
 
-  validates :amount,      presence: true,
-                          numericality: {
-                            only_integer: true,
-                            greater_than: 0
-                          }
+  validates :amount,        presence: true,
+                            numericality: {
+                              only_integer: true,
+                              greater_than_or_equal_to: :breakpoint_amount
+                            }
+
+  validates :breakpoint_id, presence: true
+
+  before_validation :set_amount
+
+  def breakpoint_amount
+    breakpoint.amount
+  end
+
+  def set_amount
+    self.amount = breakpoint.amount if self.amount.to_s.empty?
+  end
 
 end
