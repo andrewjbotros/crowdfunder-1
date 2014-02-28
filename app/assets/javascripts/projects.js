39078c2f0156888346250001032162f8baba363b
Scott Chu <domaru.tdot@gmail.com>
2014-02-28 11:39:34 -0500

    Removed duplicated function from project.js and updated activities.css

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,16 +1,6 @@
 $(document).on('page:change', function() {
 
   // if under project show page
-  if ($('.project-page').length) {
-    $('.pledged-graph').knob({
-      'readOnly': true,
-      'thickness': '0.50',
-      'fgColor': 'rgba(80, 200, 200, 0.5)',
-      'inputColor': 'rgba(20, 140, 140, 1)',
-      'draw': function() { $(this.i).val(this.cv + '%') }
-    }).trigger('change');
-
-  }
 
   if ($('.project-feed').length) {
 
