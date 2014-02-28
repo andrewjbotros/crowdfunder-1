2caf11775c7073912f644fd6d63bf85447477b4e
Scott Chu <domaru.tdot@gmail.com>
2014-02-28 11:40:08 -0500

    Add total_pledged method to breakpoint.rb

--- app/models/breakpoint.rb
+++ app/models/breakpoint.rb
@@ -14,6 +14,12 @@ class Breakpoint < ActiveRecord::Base
   validates :description,   presence: true
   validate :valid_amount
 
+  def total_pledged
+    amount * pledges.count
+  end
+
+private
+
   def valid_amount
   	if amount > project.goal
   	errors[:base] << "Breakpoint amount cannot be greater than the project goal"
