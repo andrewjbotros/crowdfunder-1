e80c6f363733ea84c713d571b40d0edbb77bd5fc
Scott Chu <domaru.tdot@gmail.com>
2014-03-02 13:22:11 -0500

    Removed Header added slide menu on the side

---
+++ app/assets/javascripts/sidebar.js
@@ -0,0 +1,43 @@
+$(document).on('page:change', function() {
+  var siteMenu = new SiteMenu();
+});
+
+function SiteMenu() {
+  var self = $('#site-menu'),
+      toggleBtn = self.children('.toggle-btn');
+
+  function currentlyHidden() {
+    return self.offset().left < 0
+  }
+
+  function slideAnimate(position, duration) {
+    duration = duration || 1000;
+    self.animate({
+      left: position
+    }, duration, 'easeOutCubic');
+  }
+
+  function slideToggle() {
+    if (currentlyHidden()) {
+      slideAnimate('0');
+    }
+    else {
+      slideAnimate('-200');
+    }
+  };
+
+  function clickableArea() {
+    return toggleBtn;
+  }
+
+  function clickEvent(btn) {
+    btn.on('click', function() {
+      $(this).unbind('click');
+      slideToggle();
+      clickEvent(clickableArea());
+    })
+  }
+
+  clickEvent(clickableArea());
+
+}
